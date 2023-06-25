650ad804ffc273787e56257906838ccf5092bb9f
dannyHallo <ruitian0716@gmail.com>
2023-06-25 23:40:52 +0800

    2

--- src/main.cc
+++ src/main.cc
@@ -5,22 +5,10 @@ using namespace std;
 
 class TestClass {
 private:
-  
 public:
-  TestClass(string c) : s(c) { cout << "testClass, " << s << " constructor" << endl; }
-
-  ~TestClass() { cout << "testClass, " << s << " destructor" << endl; }
-
 };
 
-void foo() {
-  TestClass test("foo");
-  test.print();
-}
-
 int main() {
-  foo();
-
   system("pause");
   return 0;
 }
