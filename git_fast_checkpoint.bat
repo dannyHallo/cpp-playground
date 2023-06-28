e9e0e5379ff458a9d2b44d248241512a11a3b47a
dannyHallo <ruitian0716@gmail.com>
2023-06-29 02:33:13 +0800

    CKPT_A258

--- git_fast_checkpoint.bat
+++ git_fast_checkpoint.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
-set "string=0123456789abcdef"
+set "string=0123456789ABCDEF"
 
 set /a x=%random% %% 16 
 set no1=%result%!string:~%x%,1!
@@ -10,12 +10,8 @@ set /a x=%random% %% 16
 set no3=%result%!string:~%x%,1!
 set /a x=%random% %% 16 
 set no4=%result%!string:~%x%,1!
-set /a x=%random% %% 16 
-set no5=%result%!string:~%x%,1!
-set /a x=%random% %% 16 
-set no6=%result%!string:~%x%,1!
 
-set result=CKPT_%no1%%no2%%no3%%no4%%no5%%no6%
+set result=CKPT_%no1%%no2%%no3%%no4%
 echo %result%
 
 git status
