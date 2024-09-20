5cc2b431a0f0f2a62aff5cff7b295dcf04499a4c
ruitian <yangruitian.yrt@bytedance.com>
2024-09-20 23:14:42 +0800

    better adaption to different platforms, standardization

--- compile_and_run.bat
+++
@@ -1,9 +0,0 @@
-@echo ***********************  cmake ***********************
-mkdir build
-cd build
-cmake ..
-cd ..
-
-MSBuild ./build/CCPlayground.sln -p:Configuration=Debug
-
-start /d "./build/Debug" CCPlayground.exe
